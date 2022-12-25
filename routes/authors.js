9bfbfade5c93f83c35f041b616427082d3a7dcd4
hasib <abdulhasibn@xbi4.com>
2022-12-25 14:58:11 +0530

    Add authors list

--- routes/authors.js
+++ routes/authors.js
@@ -2,16 +2,35 @@ const express = require("express")
 const router = express.Router()
 const Author =  require('../models/author')
 
-router.get("/",  (req, res) => {
-    res.render("authors/index")
+router.get("/",  async (req, res) => {
+    try{
+        const authors = await Author.find({})
+        res.render("authors/index", {
+            authors:authors
+        })
+    } catch {
+        res.redirect('/')
+    }
 })
 
 router.get("/new",  (req, res) => {
     res.render("authors/new", {author: new Author()})
 })
 
-router.post("/",  (req, res) => {
-    res.send("create")
+router.post("/",  async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try{
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    }
+    catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: "Error creating the author"
+        })
+    }
 })
 
 module.exports = router
